dad907209c9bc0b49e9512deca4350f21def8294
codebytunde <yahayaabubakarbabatunde@gmail.com>
2022-09-08 19:12:00 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* Printing a c program using the put function */
+
+/**
+ *   * main - Entry point
+ *     *
+ *       * Return: 0 on success
+ *         */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
