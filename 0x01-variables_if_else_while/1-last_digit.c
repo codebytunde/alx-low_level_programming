54e5a67092b5bbe16e0761ebe83ade9d496eb3a9
codebytunde <yahayaabubakarbabatunde@gmail.com>
2022-09-10 13:31:06 -0700

    Digit task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ if (num > 5)
 printf("Last digit of %d is %d and is greater than 5\n", num);
 else if (num == 0)
 printf("Last digit of %d is %d and is 0\n", num);
-else if (num < 6 && m !0)
+else if (num < 6 && num !0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", num);
 return (0);
 }
