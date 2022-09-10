83c8842ec6bfe8fd7ca78df28ba0393a836790f0
codebytunde <yahayaabubakarbabatunde@gmail.com>
2022-09-10 14:19:50 -0700

    Mandatory task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+int char;
+for (char = 'a'; char <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar ('\n');
+return (0);
+}
