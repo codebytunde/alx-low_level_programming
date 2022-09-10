dff3f80a7fa13b1b3567d9ced27744d65a59a1f5
codebytunde <yahayaabubakarbabatunde@gmail.com>
2022-09-10 15:30:17 -0700

    Mandatory

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints char below 10
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; a++)
+{
+putchar(num + '0');
+}
+putchar('\n');
+return (0);
+}
