dafc740d904f03fc3b740d91e997992f2f2840d3
codebytunde <yahayaabubakarbabatunde@gmail.com>
2022-09-19 08:31:26 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of n to 98
- * @n: integer pointer to a variable to be updated
- *
- * Return: nothing
+ * reset_to_98 - reset argument to 98
+ * @n: pointer
+ * Return: void
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
+	
 }
