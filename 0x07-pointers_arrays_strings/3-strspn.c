c3f28ae734aece5e4fcaa3800a3e47da686f2573
codebytunde <yahayaabubakarbabatunde@gmail.com>
2022-09-27 06:52:06 -0700

    task

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strchr - find a character in a string
+ * @s: pointer to the string to be searched
+ * @c: character to be found
+ *
+ * Return: pointer to the first occurrence of the character c
+ * dubbed by asiwaju
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
