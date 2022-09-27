325718bd417deae7e678a1f77bfe64c1f30aefb3
codebytunde <yahayaabubakarbabatunde@gmail.com>
2022-09-27 06:59:21 -0700

    task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,16 +1,35 @@
-
-#include <string.h>
 #include "main.h"
 
 /**
- * _strchr - find a character in a string
- * @s: pointer to the string to be searched
- * @c: character to be found
+ * _strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
  *
- * Return: pointer to the first occurrence of the character c
- * dubbed by asiwaju
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  */
-char *_strchr(char *s, char c)
+unsigned int _strspn(char *s, char *accept)
 {
-	return (strchr(s, c));
+	int i, j, f, flag;
+
+	f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				f++;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			return (f);
+		}
+	}
+
+	return (0);
 }
