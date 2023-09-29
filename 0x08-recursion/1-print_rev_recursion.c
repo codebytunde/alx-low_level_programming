668f955ef215d7fe2a2a993f620cecfd8baf771e
codebytunde <yahayaabubakarbabtunde@gmail.com>
2023-09-28 23:33:10 -0700

    Print reverse recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,16 +2,13 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- * @s: the string to be printed
+ * @s: string to print
  */
-
 void _print_rev_recursion(char *s)
 {
-	
-	if (s == NULL || *s == '\0')
-		return;
-
-	_print_rev_recursion(s + 1);
-
-	_putchar(*s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
