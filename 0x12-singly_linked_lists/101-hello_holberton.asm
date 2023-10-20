da4289dd4fd910e9a163c9d4689cd7aea4c40155
codebytunde <yahayaabubakarbabatunde@gmail.com>
2023-10-19 20:01:14 -0700

    new addition

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,15 @@
-section .data
-    hello db 'Hello, Holberton',10,0
-    format db '%s',10,0
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-    global main
-
-    extern printf
-
+	SECTION .text
+	extern printf
+	global main
 main:
-    push rbp
-    mov rdi, format
-    mov rsi, hello
-    call printf
-    pop rbp
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-    ; Exit the program
-    mov rax, 60                ; syscall number for exit
-    xor rdi, rdi               ; Status code (0)
-    syscall
-
+	mov eax, 0
+	ret
