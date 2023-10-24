72593d3b6e2226dd8a8fa2cbb1e245a3317c25b1
codebytunde <yahayaabubakarbabatunde@gmail.com>
2023-10-23 17:30:10 -0700

    correction to 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * free_listp - frees a linked list
@@ -32,15 +33,17 @@ void free_listp(listp_t **head)
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
+	listp_t *hptr = NULL;
+	listp_t *new, *add;
 
-	hptr = NULL;
 	while (head != NULL)
 	{
 		new = malloc(sizeof(listp_t));
-
 		if (new == NULL)
+		{
+			free_listp(&hptr);
 			exit(98);
+		}
 
 		new->p = (void *)head;
 		new->next = hptr;
@@ -66,3 +69,4 @@ size_t print_listint_safe(const listint_t *head)
 	free_listp(&hptr);
 	return (nnodes);
 }
+
