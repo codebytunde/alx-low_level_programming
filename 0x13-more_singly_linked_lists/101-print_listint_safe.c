b1f37098b53f51c8609b7e5a7308a03245f8b8fb
codebytunde <yahayaabubakarbabatunde@gmail.com>
2023-10-23 17:32:38 -0700

    correction to 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,72 +1,36 @@
 #include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * free_listp - frees a linked list
- * @head: head of a list.
+ * print_listint_safe - prints a listint_t linked list safely
+ * @head: a pointer to the head of the list
  *
- * Return: no return.
- */
-void free_listp(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * print_listint_safe - prints a linked list.
- * @head: head of a list.
- *
- * Return: number of nodes in the list.
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nnodes = 0;
-	listp_t *hptr = NULL;
-	listp_t *new, *add;
+	size_t node_count = 0;
+	const listint_t *current = head;
+	const listint_t *checker;
 
-	while (head != NULL)
+	while (current != NULL)
 	{
-		new = malloc(sizeof(listp_t));
-		if (new == NULL)
+		node_count++;
+		printf("[%p] %d\n", (void *)current, current->n);
+		checker = head;
+		for (size_t i = 0; i < node_count; i++)
 		{
-			free_listp(&hptr);
-			exit(98);
-		}
-
-		new->p = (void *)head;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-		while (add->next != NULL)
-		{
-			add = add->next;
-			if (head == add->p)
+			if (current == checker)
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listp(&hptr);
-				return (nnodes);
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				exit(98);
 			}
+			checker = checker->next;
 		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		nnodes++;
+		current = current->next;
 	}
 
-	free_listp(&hptr);
-	return (nnodes);
+	return (node_count);
 }
 
